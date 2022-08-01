dc8d6f54383a4d17da6f70a48c61dbaa5b66d924
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-01 16:20:46 -0500

    created local states and onClick function for lodgingForm

--- src/components/LodgingForm/LodgingForm.jsx
+++ src/components/LodgingForm/LodgingForm.jsx
@@ -1,23 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function LodgingForm() {
+
+    const dispatch = useDispatch();
+
+    //input local states
+    const [date, setDate] = useState('');
+    const [place, setPlace] = useState('');
+    const [lat, setLat] = useState('');
+    const [lng, setLng] = useState('');
+    const [details, setDetails] = useState('');
+
+    const handleSubmit = () => {
+        dispatch({type: 'SET_LODGING', payload: {date, place, details, lat, lng}});
+        
+        //clear inputs
+        setDate('');
+        setPlace('');
+        setLat('');
+        setLng('');
+        setDetails('');
+    }
 
 
 
     return(
         <div>
             <h1>This is the lodging form</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h4>Date:</h4>
-                <input placeholder="date" />
+                <input placeholder="date" value={date} onChange={(event) => setDate(event.target.value)} />
+
                 <h4>Place:</h4>
-                <input placeholder="place" />
+                <input placeholder="place" value={place} onChange={(event) => setPlace(event.target.value)} />
+
                 <h4>Latitude:</h4>
-                <input placeholder="latitude" />
+                <input placeholder="latitude" value={lat} onChange={(event) => setLat(event.target.value)} />
+
                 <h4>Longitude:</h4>
-                <input placeholder="longitude" />
+                <input placeholder="longitude" value={lng} onChange={(event) => setLng(event.target.value)} />
+
                 <h4>Details:</h4>
-                <input placeholder="details" />
+                <input placeholder="details" value={details} onChange={(event) => setDetails(event.target.value)} />
+
+                <button type='submit' >Submit</button>
             </form>
         </div>
     )
