9842c41cd7d607e22d8ffc07dbd8a905ac80345f
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-03 19:32:43 -0500

    filtering by trip id in other get

--- server/routes/other.router.js
+++ server/routes/other.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    let queryText = `SELECT * FROM "other";`;
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    let queryText = `SELECT * FROM "other" WHERE "trip_id" = ${id};`;
     pool.query(queryText)
     .then(result => {
         res.send(result.rows);
