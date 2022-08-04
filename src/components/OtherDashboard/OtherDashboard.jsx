7761a362492cfe3022a179cbcd950e6979a5448e
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-04 13:43:27 -0500

    added MUI to other dashboard

--- src/components/OtherDashboard/OtherDashboard.jsx
+++ src/components/OtherDashboard/OtherDashboard.jsx
@@ -3,6 +3,16 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+import { pink, red, lime, amber } from '@mui/material/colors';
+import { CardMedia } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
 import TripDashboard from "../TripDashboard/TripDashboard";
 
 function OtherDashboard() {
@@ -30,21 +40,39 @@ function OtherDashboard() {
         useEffect(() => {
         dispatch({type: 'GET_OTHER', payload: tripId})
     }, [])
+
+    const CustomizedButton = styled(Button)`
+    color: ##eeff41;
+
+    :hover {
+        color: ##c6ff00;
+    }
+    `;
     return(
         <div>
 
             <TripDashboard />
 
-            {otherArray.map(other => (
-                <div key={other.id}>
-                    <h4>Date: {other.pretty_date}</h4>
-                    <h4>Place: {other.place}</h4>
-                    <h4>Details: {other.details}</h4>
-                    <button onClick={() => handleOtherEdit(other.id)}>Edit</button>
-                    <button onClick={() => handleOtherDelete(other.id, tripId)}>Delete</button>
-                </div>
-                )
-            )}
+            <Card sx={{ maxWidth: 345, m:2, background: pink[500] }} >
+                {otherArray.map(other => (
+                    <div key={other.id}>
+                        <Typography variant="body1" color="text.primary" align="center">
+                            Date: {other.pretty_date}
+                        </Typography>
+                        <Typography variant="body1" color="text.primary" align="center">
+                            Place: {other.place}
+                        </Typography>
+                        <Typography variant="body1" color="text.primary" align="center">
+                            Details: {other.details}
+                        </Typography>
+
+                        <Button variant="constrained" onClick={() => handleOtherEdit(other.id)}>Edit</Button>
+
+                        <Button variant="contained" onClick={() => handleOtherDelete(other.id, tripId)}>Delete</Button>
+                    </div>
+                    )
+                )}
+            </Card>
         </div>
     )
 }
