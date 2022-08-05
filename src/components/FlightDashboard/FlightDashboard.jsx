5776fe8b25cfa6d0b3bcc0cc9722cd6064004c40
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-05 14:16:15 -0500

    added mui cards to flightDashboard

--- src/components/FlightDashboard/FlightDashboard.jsx
+++ src/components/FlightDashboard/FlightDashboard.jsx
@@ -3,6 +3,14 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { pink, red, lime, amber } from '@mui/material/colors';
+import { CardMedia } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
 import TripDashboard from "../TripDashboard/TripDashboard";
 
 function FlightDashboard() {
@@ -34,9 +42,8 @@ function FlightDashboard() {
         <div>
             <TripDashboard />
 
-            {flightsArray.map(flight => (
-                <div key={flight.id}>
-                    <h4>Date: {flight.pretty_date}</h4>
+
+                    {/* <h4>Date: {flight.pretty_date}</h4>
                     <h4>Airline: {flight.airline}</h4>
                     <h4>Arrival Time: {flight.put_arrival}</h4>
                     <h4>Departure Time: {flight.put_departure}</h4>
@@ -45,7 +52,36 @@ function FlightDashboard() {
                     <button onClick={() => handleFlightDelete(flight.id, tripId)}>Delete</button>
                 </div>
                 )
-            )}
+            )} */}
+
+            {flightsArray.map(flight => (
+                <div key={flight.id}>
+                        <Card sx={{ maxWidth: 345, m:2, background: lime[300] }} >
+
+                            <Typography fontWeight={'bold'} variant="body1" color="text.primary" align="center" marginTop={1}>
+                                {flight.pretty_date}
+                            </Typography>
+                            <Typography fontWeight={'medium'} variant="body1" color="text.primary" align="center" margin={2}>
+                                {flight.airline}
+                            </Typography>
+                            <Typography variant="body2" color="text.primary" align="center">
+                                Departure:  {flight.put_departure}
+                            </Typography>
+                            <Typography variant="body2" color="text.primary" align="center">
+                            Arrival:  {flight.put_arrival}
+                            </Typography>
+                            <Typography variant="body2" color="text.primary" align="left" marginLeft={4}>
+                                {flight.flight_number}
+                            </Typography>
+
+                            <div align="center">
+                                <Button sx={{background: pink[400], m:2}} variant="contained" onClick={() => handleHikeEdit(hike)}>Edit</Button>
+                                <Button sx={{background: pink[400], m:2}} variant="contained" onClick={() => handleHikeDelete(hike.id, tripId)}>Delete</Button>
+                            </div>
+                        </Card>
+                    </div>
+                    )
+                )}
         </div>
     )
 }
