480d9a89e5700f5b233baa5b6ccfa08441dc047b
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-08 11:28:42 -0500

    added authorization to hike router

--- server/routes/hike.router.js
+++ server/routes/hike.router.js
@@ -23,49 +23,62 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const { date, place, details, tripId} = req.body
-    console.log('req.body in hike post', req.body);
-    console.log('date, place, details, tripId in hike router.post', date, place, details, tripId);
-    let queryText = `INSERT INTO "hike" 
-        ("date", "place", "details", "trip_id")
-        VALUES ($1, $2, $3, $4);`;
-    pool.query(queryText, [date, place, details, tripId])
-    .then(() => {
-        res.sendStatus(200);
-    }).catch(error => {
-        console.log('error in hike router.post', error);
-    })
+
+    //if the user is an admin, allow them to post
+    if (req.user.clearance === 2) {
+        const { date, place, details, tripId} = req.body
+        console.log('req.body in hike post', req.body);
+        console.log('date, place, details, tripId in hike router.post', date, place, details, tripId);
+        let queryText = `INSERT INTO "hike" 
+            ("date", "place", "details", "trip_id")
+            VALUES ($1, $2, $3, $4);`;
+        pool.query(queryText, [date, place, details, tripId])
+        .then(() => {
+            res.sendStatus(200);
+        }).catch(error => {
+            console.log('error in hike router.post', error);
+        })
+    }else {
+        res.sendStatus(403);
+    }
 });
 
 router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    const queryText = `DELETE FROM "hike" WHERE "id" = $1`;
-    pool
-    .query(queryText, [id])
-    .then(() => {
-        res.sendStatus(201);
-    })
-    .catch((error) => {
-        console.log('error in hike router.delete', error);
-        res.sendStatus(500);
-    })
-    
+    if (req.user.clearance === 2) {
+        const id = req.params.id;
+        const queryText = `DELETE FROM "hike" WHERE "id" = $1`;
+        pool
+        .query(queryText, [id])
+        .then(() => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log('error in hike router.delete', error);
+            res.sendStatus(500);
+        })
+    } else {
+        res.sendStatus(403);
+    }   
 })
 
 router.put('/:id', (req, res) => {
-    // Update this single hike
-    console.log('req.params', req.params)
-    const idToUpdate = req.params.id;
-    console.log('req.body', req.body);
-    const sqlText = `UPDATE "hike" SET "date" = $1, "place" = $2, "details" = $3 WHERE id = $4`;
-    pool.query(sqlText, [req.body.put_date, req.body.place, req.body.details, idToUpdate])
-        .then((result) => {
-            res.sendStatus(200);
-        })
-        .catch((error) => {
-            console.log(`Error making database query ${sqlText}`, error);
-            res.sendStatus(500);
-        });
+    if (req.user.clearance === 2) {
+        // Update this single hike
+        console.log('req.params', req.params)
+        const idToUpdate = req.params.id;
+        console.log('req.body', req.body);
+        const sqlText = `UPDATE "hike" SET "date" = $1, "place" = $2, "details" = $3 WHERE id = $4`;
+        pool.query(sqlText, [req.body.put_date, req.body.place, req.body.details, idToUpdate])
+            .then((result) => {
+                res.sendStatus(200);
+            })
+            .catch((error) => {
+                console.log(`Error making database query ${sqlText}`, error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
