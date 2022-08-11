c75460ef68cac49dd969371ee3bb5478752b8dbd
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-11 10:09:20 -0500

    styled lodging dashboard

--- src/components/LodgingDashboard/LodgingDashboard.jsx
+++ src/components/LodgingDashboard/LodgingDashboard.jsx
@@ -7,6 +7,7 @@ import mapboxgl from '!mapbox-gl';
 
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { pink, red, lime, amber, orange } from '@mui/material/colors';
@@ -61,28 +62,30 @@ function LodgingDashboard() {
 
                 {lodgingArray.map(lodgingItem => (
                     <div key={lodgingItem.id}>
-                        <Card sx={{ maxWidth: 345, m:2, background: "#FA9500"}}  >
+                        <Card sx={{ maxWidth: 345, m:2, background: "#BB4711"}}  >
 
-                            <Typography sx={{borderRadius:2, m:2, p:2, background: "#FDF6C3", border: "5px solid #BB4711"}} fontWeight={'bold'} variant="body1" color="black" align="center" marginTop={1}>
-                                {lodgingItem.pretty_date}
-                            </Typography>
-                            <Typography fontWeight={'medium'} variant="body1" color="text.primary" align="center" margin={2}>
-                                {lodgingItem.place}
-                            </Typography>
+                            <Box sx={{borderRadius:2, m:2, p:2, background: "#FDF6C3", border: "5px solid #FF9D0A"}}>
+                                <Typography fontWeight={'bold'} variant="body1" color="black" align="center" marginTop={1}>
+                                    {lodgingItem.pretty_date}
+                                </Typography>
+                                <Typography fontWeight={'medium'} variant="body1" color="text.primary" align="center" margin={2}>
+                                    {lodgingItem.place}
+                                </Typography>
+                            </Box>
 
 
-                            <CardMedia sx={{p:2, background: "#FFAF85"}} align="center" >
+                            <CardMedia sx={{p:2, background: "#FDF6C3"}} align="center" >
                             <img className="map-image" onClick={() => handleMapClick(lodgingItem)} src={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/pin-l+ff00ae(${lodgingItem.longitude},${Number(lodgingItem.latitude)})/${Number(lodgingItem.longitude)},${Number(lodgingItem.latitude)},9.04,0/300x200?access_token=pk.eyJ1IjoicmFjaHkyMjkiLCJhIjoiY2w2NTYwb3F5MnhuYjNjbzEyam84MzkzcCJ9.uLkhGXRBZOcb2rzrggZePQ`} />
                             </CardMedia>
 
-                            <Typography sx={{borderRadius:2, m:2, p:2, background: "#FDF6C3", border: "5px solid #FFAF85"}} variant="body2" color="text.primary" align="left" marginLeft={4}>
+                            <Typography sx={{borderRadius:2, m:2, p:2, background: "#FDF6C3", border: "5px solid #FE5F55"}} variant="body2" color="text.primary" align="left" marginLeft={4}>
                                 {lodgingItem.details}
                             </Typography>
 
                             {user.clearance === 2 ? (
                                 <div align="center">
-                                    <Button sx={{background: "#7C6A0A", m:2}} variant="contained" onClick={() => handleLodgingEdit(lodgingItem)}>Edit</Button>
-                                    <Button sx={{background: "#7C6A0A", m:2}} variant="contained" onClick={() => handleLodgingDelete(lodgingItem.id, tripId)}>Delete</Button>
+                                    <Button sx={{background: "#6F1A07", m:2}} variant="contained" onClick={() => handleLodgingEdit(lodgingItem)}>Edit</Button>
+                                    <Button sx={{background: "#6F1A07", m:2}} variant="contained" onClick={() => handleLodgingDelete(lodgingItem.id, tripId)}>Delete</Button>
                                 </div>
                             ) : (
                                 <></>
